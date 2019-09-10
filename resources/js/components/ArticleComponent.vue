8ec949ec4e8802e19fce728c7ba3a4ed64df2724
itep dj <itep.dj@gmail.com>
2019-09-10 16:12:00 +0700

    add article vue js

--- resources/js/components/ArticleComponent.vue
+++ resources/js/components/ArticleComponent.vue
@@ -2,6 +2,16 @@
     <div class="container">
         <h2>Blog Post</h2>
 
+        <form @submit.prevent="addArticle" class="mb-3">
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="Title" v-model="article.title">
+            </div>
+            <div class="form-group">
+                <textarea class="form-control" placeholder="Body" v-model="article.body"></textarea>
+            </div>
+            <button type="submit" class="btn btn-light btn-block">Save</button>
+        </form>
+
         <nav aria-label="Page navigation example">
         <ul class="pagination">
             <li v-bind:class="[{disabled: !pagination.prev_page_url}]" class="page-item">
@@ -21,11 +31,15 @@
         <div class="card card-body mb-2" v-for="article in articles" v-bind:key="article.id">
             <h3>{{ article.title }}</h3>
             <p>{{ article.body }}</p>
+            <hr>
+
+            <button @click="deleteArticle(article.id)" class="btn btn-danger">Delete</button>
         </div>
     </div>
 </template>
 
 <script>
+import { resolve } from 'dns';
     export default {
         data() {
             return {
@@ -68,6 +82,43 @@
                 }
 
                 this.pagination = pagination;
+            },
+
+            deleteArticle(id) {
+                if(confirm('Are you sure?')) {
+                    fetch(`api/article/${id}`, {
+                        method: 'delete'
+                    })
+                    .then(res => res.json())
+                    .then(data => {
+                        alert('Article removed');
+                        this.fetchArticles();
+                    })
+                    .catch(err => console.log(err));
+                }
+            },
+            
+            addArticle() {
+                if(this.edit === false) {
+                    // add
+                    fetch('api/article', {
+                        method: 'post',
+                        body: JSON.stringify(this.article),
+                        headers: {
+                            'content-type': 'application/json'
+                        }
+                    })
+                    .then(res => res.json())
+                    .then(data => {
+                        this.article.title = '';
+                        this.article.body = '';
+                        alert('Article added');
+                        this.fetchArticles();
+                    })
+                    .catch(err => console.log(err));
+                } else {
+                    // edit
+                }
             }
         }
     }
